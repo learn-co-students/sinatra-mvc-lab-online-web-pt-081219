9ad461dae8f5e6bf99ab55b703e93bb291dc3633
Alec Hall <alecnhall@gmail.com>
2019-11-19 20:08:52 -0700

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,23 @@
+class PigLatinizer 
+    def piglatinize(input)
+        words = input.split(" ")
+    
+        words.map do |word|
+          piglatinize_word(word)
+        end.join(" ")
+      end
+    
+      private
+      
+      def piglatinize_word(input)
+        first = input.match(/^[^AEIOUYaeiouy]+/)
+    
+        if first
+          first = first[0]
+          rest = input[first.length..-1]
+          "#{rest}#{first}ay"
+        else
+          "#{input}way"
+        end
+      end
+end
