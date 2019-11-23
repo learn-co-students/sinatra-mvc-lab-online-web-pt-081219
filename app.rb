53a1fa0634ccef0811ee15bdc39e149aa68d1fd3
cep104 <candicelizabethpeters@gmail.com>
2019-11-22 18:28:10 -0600

    finished
    yes!

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     end
     
     post '/piglatinize' do
-        @user_phrase = PigLatinizer.new.splits_the_sentence(params[:user_phrase])
+        @user_phrase = PigLatinizer.new.piglatinize(params[:user_phrase])
         erb :result
     end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,15 +4,19 @@ class PigLatinizer
         vowels.include?(word[0])
     end
 
-    def piglatinize(word)
-        if self.is_vowel?(word)
-            word.concat('way')
-        else
-            word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
-        end
-    end
+    def piglatinize(string)
+        words = string.split
+        words.collect do |word|
+          if is_vowel?(word[0])
+            word += "way"
+          else
+            until is_vowel?(word[0]) do
+              word = word[1..-1] + word[0]
+            end
+            word = word + "ay"
+          end
+        end.join(" ")
+      end
 
-    def splits_the_sentence(phrase)
-        phrase.split.collect {|word| piglatinize(word)}.join(' ')
-    end
+  
 end
