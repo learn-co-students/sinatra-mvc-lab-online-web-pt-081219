59f5f5743a465ad5c6714ba78782c197fc267d26
CleanSolLLC <mamark90@gmail.com>
2019-11-26 23:35:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+	get "/" do	  
+	  erb :user_input
+	end
+
+	post "/piglatinize" do
+		piglatin = PigLatinizer.new
+		@convert_to_piglatin = piglatin.piglatinize(params[:user_phrase])
+		erb :user_output
+	end
+
+
 end
