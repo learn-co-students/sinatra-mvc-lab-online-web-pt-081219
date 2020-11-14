f5b045288e8c63c4129cc0e8d4404b149dc24ff6
Candice Peters <51180851+cep104@users.noreply.github.com>
2020-11-14 00:58:28 -0600

    Update piglatinizer.rb
    notes

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,17 +5,17 @@ class PigLatinizer
     end
 
     def piglatinize(string)
-        words = string.split
-        words.collect do |word|
-          if is_vowel?(word[0])
-            word += "way"
-          else
-            until is_vowel?(word[0]) do
-              word = word[1..-1] + word[0]
+        words = string.split #turn into an array 
+        words.collect do |word| #itterate over the array of words
+          if is_vowel?(word[0]) #example apple
+            word += "way" #appleway
+          else #example word bacon
+            until is_vowel?(word[0]) do #until a letter in a word is a vowel (acon)
+              word = word[1..-1] + word[0] #letters 2- end of rest of letters plus the first letter at the end (aconb)
             end
-            word = word + "ay"
+            word = word + "ay" #add ay at end of word aconbay
           end
-        end.join(" ")
+        end.join(" ") #do this to all words then join with a space and make a string again
       end
 
   
